844261f198c68da4a0622e89d3c25fc9ebf0df35
ChyYasir <chyyasir2000@gmail.com>
2023-12-13 22:53:18 +0600

    dark on refresh

--- src/components/hooks/useThemeSwitcher.js
+++ src/components/hooks/useThemeSwitcher.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const useThemeSwitcher = () => {
   const preferDarkQuery = "(prefers-color-scheme: dark)";
-  const [mode, setMode] = useState("");
+  const [mode, setMode] = useState("light");
 
   useEffect(() => {
     const mediaQuery = window.matchMedia(preferDarkQuery);
